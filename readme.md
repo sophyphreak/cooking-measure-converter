1bf1a79dfd721b2e2ce0111236ef16c4a20c0b88
Andrew <arhorn@smcm.edu>
2018-04-26 11:10:30 +0700

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# React Measure Converter
+
+Simple app that converts metric to imperial/standard and vice versa.
+
+## Authors
+
+* Andrew Horn&nbsp;
+
+## License
+
+This project is licensed under the MIT License.
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RtfToMarkdown from './components/RtfToMarkdown/RtfToMarkdown';
+
+// import ReactQuill styles
+import 'react-quill/dist/quill.snow.css';
+// Import Main styles for this application
+import '../scss/style.scss';
+
+ReactDOM.render(<RtfToMarkdown />, document.getElementById('root'));
