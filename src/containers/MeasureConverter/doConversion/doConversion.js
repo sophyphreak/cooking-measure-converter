ac0e8fa57bc1e29e0f8ec1f78b64e7b5b69f3b78
Andrew <arhorn@smcm.edu>
2018-04-26 23:04:38 +0700

    Added doConversion method file

---
+++ src/containers/MeasureConverter/doConversion/doConversion.js
@@ -0,0 +1,13 @@
+import convert from 'convert-units';
+import roundTo from 'round-to';
+
+const doConversion = conversionInputs => {
+  const {
+    inputAmount,
+    inputUnit,
+    outputUnit
+  } = conversionInputs;
+  let outputAmount = convert(inputAmount).from(inputUnit).to(outputUnit);
+  outputAmount = roundTo(outputAmount, 2);
+  return outputAmount;
+};
