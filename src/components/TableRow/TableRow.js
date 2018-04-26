b4d8cc795eeecaea4a07203fcb37052f4f630275
Andrew <arhorn@smcm.edu>
2018-04-26 22:30:03 +0700

    Added skeleton for table row

--- src/components/TableRow/TableRow.js
+++ src/components/TableRow/TableRow.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const TableRow = props => <div />;
+const TableRow = props => (
+  <tr>
+    <th>{/*metric dropdown*/}</th>
+    <td>{/*metric input*/}</td>
+    <td>{/*imperial input*/}</td>
+    <th>{/*imperial dropdown*/}</th>
+  </tr>
+);
 
 export default TableRow;
