35cfb682fb3f2d97bc99f2ae27479ccb2fbff519
Andrew Horn <arhorn@smcm.edu>
2019-05-30 22:01:53 -0600

    added name to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,30 +5,34 @@ import { Button, Col, Form, FormGroup, Input, Label, Row } from 'reactstrap'
 const ContactPage = () => (
   <Row className="main contactForm animated fadeIn">
   <Col xs={{ size: 10, offset: 1 }} md={{ size: 8, offset: 2 }} lg={{ size: 4, offset: 4 }}>
-  <br />
-      <Form name="contact" method="POST" data-netlify="true">
-        <FormGroup>
-          <Label>your email address.</Label>
-          <Input type="email" name="email" />
-        </FormGroup>
-        <FormGroup>
-          <Label>the short version.</Label>
-          <Input type="text" name="subject" />
-        </FormGroup>
-        <FormGroup>
-          <Label>what you want to tell me.</Label>
-          <Input type="textarea" name="message" />
-        </FormGroup>
-        <Button type="submit" color="primary">
-          Submit
+    <br />
+    <Form name="contact" method="POST" data-netlify="true">
+      <FormGroup>
+        <Label>your name</Label>
+        <Input type="text" name="name" />
+      </FormGroup>
+      <FormGroup>
+        <Label>your email address</Label>
+        <Input type="email" name="email" />
+      </FormGroup>
+      <FormGroup>
+        <Label>the short version</Label>
+        <Input type="text" name="subject" />
+      </FormGroup>
+      <FormGroup>
+        <Label>what you want to tell me</Label>
+        <Input type="textarea" name="message" />
+      </FormGroup>
+      <Button type="submit" color="primary">
+        Submit
+      </Button>
+      </Form>
+      <br />
+      <Link to="/">
+        <Button className="align-center" color="link">
+          return to cooking measure converter
         </Button>
-        </Form>
-        <br />
-        <Link to="/">
-          <Button className="align-center" color="link">
-            return to cooking measure converter
-          </Button>
-        </Link>
+      </Link>
   </Col>
 </Row>
 )
