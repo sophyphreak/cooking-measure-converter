a9eca3395086eb54d8a7dc76e759fdebedea2580
Andrew Horn <arhorn@smcm.edu>
2019-05-30 22:22:51 -0600

    Added netlify honeypot

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { Button, Col, Form, FormGroup, Input, Label, Row } from 'reactstrap'
+import { Button, Col, Form, FormGroup, Input, Label, Row } from 'reactstrap';
 
 const ContactPage = () => (
   <Row className="main contactForm animated fadeIn">
-  <Col xs={{ size: 10, offset: 1 }} md={{ size: 8, offset: 2 }} lg={{ size: 4, offset: 4 }}>
-    <br />
-    <Form name="contact" method="POST" data-netlify="true">
-      <FormGroup>
-        <Label>your name</Label>
-        <Input type="text" name="name" />
-      </FormGroup>
-      <FormGroup>
-        <Label>your email address</Label>
-        <Input type="email" name="email" />
-      </FormGroup>
-      <FormGroup>
-        <Label>the short version</Label>
-        <Input type="text" name="subject" />
-      </FormGroup>
-      <FormGroup>
-        <Label>what you want to tell me</Label>
-        <Input type="textarea" name="message" />
-      </FormGroup>
-      <Button type="submit" color="primary">
-        Submit
-      </Button>
+    <Col
+      xs={{ size: 10, offset: 1 }}
+      md={{ size: 8, offset: 2 }}
+      lg={{ size: 4, offset: 4 }}
+    >
+      <br />
+      <Form
+        name="contact"
+        method="POST"
+        netlify-honeypot="bot-field"
+        data-netlify="true"
+      >
+        <FormGroup hidden>
+          <Label>Don't fill this out if you're human</Label>
+          <Input name="bot-field" />
+        </FormGroup>
+        <FormGroup>
+          <Label>your name</Label>
+          <Input type="text" name="name" />
+        </FormGroup>
+        <FormGroup>
+          <Label>your email address</Label>
+          <Input type="email" name="email" />
+        </FormGroup>
+        <FormGroup>
+          <Label>the short version</Label>
+          <Input type="text" name="subject" />
+        </FormGroup>
+        <FormGroup>
+          <Label>what you want to tell me</Label>
+          <Input type="textarea" name="message" />
+        </FormGroup>
+        <Button type="submit" color="primary">
+          Submit
+        </Button>
       </Form>
       <br />
       <Link to="/">
@@ -33,8 +46,8 @@ const ContactPage = () => (
           return to cooking measure converter
         </Button>
       </Link>
-  </Col>
-</Row>
-)
+    </Col>
+  </Row>
+);
 
 export default ContactPage;
