f31688689738c5452f3ba7ea838c2767007c7ae4
Andrew Horn <arhorn@smcm.edu>
2019-05-30 21:09:03 -0600

    removed honeypot and recaptcha

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,10 +5,7 @@ import { Button, Col, Form, FormGroup, Input, Label, Row } from 'reactstrap'
 const ContactPage = () => (
   <Row className="main contactForm animated fadeIn">
   <Col xs="12" sm={{ size: 4, offset: 4 }}>
-      <Form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" data-netlify-recaptcha="true">
-        <p class="hidden">
-          <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-        </p>
+      <Form name="contact" method="POST" data-netlify="true">
         <FormGroup>
           <Label>your email address:</Label>
           <Input type="email" name="email" />
@@ -21,7 +18,6 @@ const ContactPage = () => (
           <Label>what you want to say:</Label>
           <Input type="textarea" name="message" />
         </FormGroup>
-        <div data-netlify-recaptcha="true"></div>
         <Button type="submit" color="primary">
           Submit
         </Button>
