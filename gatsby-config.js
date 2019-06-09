0ecc73dbe29693e429b7f5f8ed5d2dd947cab116
Andrew Horn <arhorn@smcm.edu>
2019-06-09 14:08:33 -0600

    made this a progressive web application

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,23 @@ module.exports = {
   siteMetadata: {
     title: `cooking measure converter`,
     description: `Convert measurements from metric to standard and back again.`,
-    author: `Andrew Horn`,
+    author: `Andrew Horn`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `cooking-measure-converter`,
+        short_name: `measure-converter`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
+    },
+    `gatsby-plugin-offline`
+  ]
+};
